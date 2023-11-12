aebdbb4a4504d4c6f926d2e073631c8bb46add14
AV2001 <anirudhvadlamani2001@gmail.com>
2023-11-13 02:21:14 +0400

    setup database

--- db.js
+++ db.js
@@ -1,1 +1,20 @@
 /** Database setup for BizTime. */
+const { Client } = require('pg');
+
+// Load environment variables
+require('dotenv').config();
+
+const dbPassword = process.env.DB_PASSWORD;
+
+const DB_URI =
+    process.env.NODE_ENV === 'test'
+        ? `postgresql://postgres:${dbPassword}@localhost:5432/usersdb_test`
+        : `postgresql://postgres:${dbPassword}@localhost:5432/usersdb`;
+
+let db = new Client({
+    connectionString: DB_URI,
+});
+
+db.connect();
+
+module.exports = db;
