1f354393fdb2522fe5087e290a8d1c9d605318f9
AV2001 <anirudhvadlamani2001@gmail.com>
2023-11-14 04:06:56 +0400

    modify the error handler

--- app.js
+++ app.js
@@ -10,21 +10,17 @@ app.use(express.json());
 app.use('/companies', companyRoutes);
 
 /** 404 handler */
-
 app.use(function (req, res, next) {
     const err = new ExpressError('Not Found', 404);
     return next(err);
 });
 
 /** general error handler */
-
 app.use((err, req, res, next) => {
-    res.status(err.status || 500);
+    let status = err.status || 500;
+    let message = err.message;
 
-    return res.json({
-        error: err,
-        message: err.message,
-    });
+    return res.json({ error: { message, status } });
 });
 
 module.exports = app;
