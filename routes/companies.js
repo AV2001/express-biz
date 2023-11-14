ea873af0f585e8320464113f2c5d74cce2eb9b41
AV2001 <anirudhvadlamani2001@gmail.com>
2023-11-14 04:36:31 +0400

    add put route to update a company

--- routes/companies.js
+++ routes/companies.js
@@ -40,4 +40,24 @@ router.post('/', async (req, res, next) => {
     }
 });
 
+router.put('/:code', async (req, res, next) => {
+    try {
+        const { code } = req.params;
+        const { name, description } = req.body;
+        const results = await db.query(
+            `UPDATE companies SET name=$1, description=$2 WHERE code=$3 RETURNING *`,
+            [name, description, code]
+        );
+        if (results.rows.length === 0) {
+            throw new ExpressError(
+                `Can't find a company whose code is ${code}.`,
+                404
+            );
+        }
+        return res.status(200).json({ company: results.rows[0] });
+    } catch (e) {
+        return next(e);
+    }
+});
+
 module.exports = router;
