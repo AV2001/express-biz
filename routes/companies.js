7df53e9541058405184319422bf58b1f99c2c638
AV2001 <anirudhvadlamani2001@gmail.com>
2023-11-14 04:09:07 +0400

    add get route to fetch a company based on code

--- routes/companies.js
+++ routes/companies.js
@@ -1,10 +1,27 @@
 const express = require('express');
 const router = express.Router();
+const ExpressError = require('../expressError');
 const db = require('../db');
 
 router.get('/', async (req, res, next) => {
-    const results = await db.query('SELECT code, name FROM companies;');
+    const results = await db.query('SELECT code, name FROM companies');
     return res.status(200).json({ companies: results.rows });
+});
+
+router.get('/:code', async (req, res, next) => {
+    try {
+        const { code } = req.params;
+        const results = await db.query(`SELECT * FROM companies WHERE code = $1`, [code]);
+        if (results.rows.length === 0) {
+            throw new ExpressError(
+                `Can't find a company whose code is ${code}.`,
+                404
+            );
+        }
+        return res.status(200).json({ company: results.rows[0] });
+    } catch (e) {
+        return next(e);
+    }
 });
 
 module.exports = router;
