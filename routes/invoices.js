39c392402d5a97afad45413d128f881169257900
AV2001 <anirudhvadlamani2001@gmail.com>
2023-11-15 09:17:00 +0400

    add get route to fetch all invoices

---
+++ routes/invoices.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+const ExpressError = require('../expressError');
+const db = require('../db');
+
+router.get('/', async (req, res, next) => {
+    const results = await db.query(`SELECT * FROM invoices`);
+    return res.status(200).json({ invoices: results.rows });
+});
+
+module.exports = router;
