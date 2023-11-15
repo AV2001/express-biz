3685a9acdc22478707f8702c429796a20ae8f746
AV2001 <anirudhvadlamani2001@gmail.com>
2023-11-15 09:21:24 +0400

    change query in get route to get only id and comp_code

--- routes/invoices.js
+++ routes/invoices.js
@@ -4,7 +4,7 @@ const ExpressError = require('../expressError');
 const db = require('../db');
 
 router.get('/', async (req, res, next) => {
-    const results = await db.query(`SELECT * FROM invoices`);
+    const results = await db.query(`SELECT id, comp_code FROM invoices`);
     return res.status(200).json({ invoices: results.rows });
 });
 
