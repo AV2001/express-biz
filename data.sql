342ea7d9d2f9a597abf0b4d2724af5d43767aa78
AV2001 <anirudhvadlamani2001@gmail.com>
2023-11-19 09:09:49 +0400

    add table for industries and companies_industries (joint table)

--- data.sql
+++ data.sql
@@ -1,28 +1,60 @@
+DROP TABLE IF EXISTS companies CASCADE;
+
 DROP TABLE IF EXISTS invoices;
-DROP TABLE IF EXISTS companies;
 
-CREATE TABLE companies (
-    code text PRIMARY KEY,
-    name text NOT NULL UNIQUE,
-    description text
-);
+DROP TABLE IF EXISTS industries CASCADE;
 
-CREATE TABLE invoices (
-    id serial PRIMARY KEY,
-    comp_code text NOT NULL REFERENCES companies ON DELETE CASCADE,
-    amt float NOT NULL,
-    paid boolean DEFAULT false NOT NULL,
-    add_date date DEFAULT CURRENT_DATE NOT NULL,
-    paid_date date,
-    CONSTRAINT invoices_amt_check CHECK ((amt > (0)::double precision))
-);
+DROP TABLE IF EXISTS companies_industries;
 
-INSERT INTO companies
-  VALUES ('apple', 'Apple Computer', 'Maker of OSX.'),
-         ('ibm', 'IBM', 'Big blue.');
+CREATE TABLE
+    companies (
+        code text PRIMARY KEY,
+        name text NOT NULL UNIQUE,
+        description text
+    );
 
-INSERT INTO invoices (comp_Code, amt, paid, paid_date)
-  VALUES ('apple', 100, false, null),
-         ('apple', 200, false, null),
-         ('apple', 300, true, '2018-01-01'),
-         ('ibm', 400, false, null);
+CREATE TABLE
+    invoices (
+        id serial PRIMARY KEY,
+        comp_code text NOT NULL REFERENCES companies ON DELETE CASCADE,
+        amt float NOT NULL,
+        paid boolean DEFAULT false NOT NULL,
+        add_date date DEFAULT CURRENT_DATE NOT NULL,
+        paid_date date
+    );
+
+CREATE TABLE
+    industries (code text PRIMARY KEY, industry text);
+
+CREATE TABLE
+    companies_industries (
+        company_code text REFERENCES companies (code),
+        industry_code text REFERENCES industries (code),
+        PRIMARY KEY (company_code, industry_code)
+    );
+
+INSERT INTO
+    companies
+VALUES
+    ('apple', 'Apple Computer', 'Maker of OSX.'),
+    ('ibm', 'IBM', 'Big blue.');
+
+INSERT INTO
+    invoices (comp_code, amt, paid, paid_date)
+VALUES
+    ('apple', 100, false, null),
+    ('apple', 200, false, null),
+    ('apple', 300, true, '2018-01-01'),
+    ('ibm', 400, false, null);
+
+INSERT INTO
+    industries (code, industry)
+VALUES
+    ('acct', 'Accounting'),
+    ('mark', 'Marketing');
+
+INSERT INTO
+    companies_industries (company_code, industry_code)
+VALUES
+    ('apple', 'acct'),
+    ('apple', 'mark')
